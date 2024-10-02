8f2a47933a41df23c0423261f370557aeb92bdb1
guillermotzuc <guillermo.tzuc@gmail.com>
2024-10-02 15:30:54 -0600

    Fix scroll and order on history

--- personaltracker/app/src/main/java/com/health/personaltracker/model/FragmentBase.java
+++ personaltracker/app/src/main/java/com/health/personaltracker/model/FragmentBase.java
@@ -2,6 +2,8 @@ package com.health.personaltracker.model;
 
 import androidx.fragment.app.Fragment;
 import androidx.room.Room;
+import androidx.room.migration.Migration;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
 import com.health.personaltracker.AppDatabase;
 import com.health.personaltracker.dao.FastingDao;
@@ -10,9 +12,20 @@ public abstract class FragmentBase extends Fragment {
 
     protected AppDatabase getAppDatabase() {
         return Room.databaseBuilder(getActivity().getApplicationContext(),
-                AppDatabase.class, "database-name").allowMainThreadQueries().build();
+                AppDatabase.class, "ayuno-app-database")
+                .allowMainThreadQueries()
+                .build();
     }
 
+    // TODO APPLY THIS FOR MIGRATION
+    private Migration from1to2() {
+        return new Migration(1, 2) {
+            @Override
+            public void migrate(SupportSQLiteDatabase db) {
+                super.migrate(db);
+            }
+        };
+    }
     protected FastingDao getFastingDao() {
         return getAppDatabase().fastingDao();
     }
