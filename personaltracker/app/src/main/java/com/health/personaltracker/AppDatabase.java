82cab55d391d403665877cc3c4898432b5fe7580
guillermotzuc <guillermo.tzuc@gmail.com>
2024-10-03 09:21:12 -0600

    Add schema version

--- personaltracker/app/src/main/java/com/health/personaltracker/AppDatabase.java
+++ personaltracker/app/src/main/java/com/health/personaltracker/AppDatabase.java
@@ -6,7 +6,7 @@ import androidx.room.RoomDatabase;
 import com.health.personaltracker.dao.FastingDao;
 import com.health.personaltracker.entity.Fasting;
 
-@Database(entities = { Fasting.class }, version = 1, exportSchema = false)
+@Database(entities = { Fasting.class }, version = 1, exportSchema = true)
 public abstract class AppDatabase extends RoomDatabase {
     public abstract FastingDao fastingDao();
 }
